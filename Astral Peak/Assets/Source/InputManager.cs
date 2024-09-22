cb6c3d9f46f31ad40ccddb9cb12f4c5c004f2619
nasrra <naserra.work@gmail.com>
2024-09-22 17:51:18 +1000

    Basic input from keyboard

--- Astral Peak/Assets/Source/InputManager.cs
+++ Astral Peak/Assets/Source/InputManager.cs
@@ -1,7 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public static class InputManager{
-    
+public class InputManager : MonoBehaviour{
+    [SerializeField] private PlayerInput input;
+    private Keybinds keybinds;
+    public void Start(){
+        // enable keyboard keybinds
+        keybinds = new Keybinds();
+        keybinds.Keyboard.Enable();
+        keybinds.Keyboard.Jump.performed += Jump;
+        keybinds.Keyboard.Left.performed += Left;
+        keybinds.Keyboard.Right.performed += Right;
+    }
+
+    public void Jump(InputAction.CallbackContext ctx){
+        Debug.Log("jump");
+    }
+
+    public void Left(InputAction.CallbackContext ctx){
+        Debug.Log("left");
+    }
+
+    public void Right(InputAction.CallbackContext ctx){
+        Debug.Log("right");
+    }
 }
