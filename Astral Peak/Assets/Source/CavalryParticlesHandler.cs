3cfbc30f19896c9518c2d6f24d9d742b1e85dbc2
nasrra <naserra.work@gmail.com>
2024-11-01 18:09:59 +1000

    [Add] wold ground slam implemented with particles and damagin functionality.

--- Astral Peak/Assets/Source/CavalryParticlesHandler.cs
+++ Astral Peak/Assets/Source/CavalryParticlesHandler.cs
@@ -6,11 +6,15 @@ public class CavalryParticlesHandler : ParticlesHandler{
         back_strike_backward    ,
         back_strike_forward     ,
         top_bite                ,
-        bottom_bite             ;
+        bottom_bite             ,
+        ground_slam_hop_first ,
+        ground_slam_hop_second;
     
     public void emit_front_strike        () => front_strike.Emit(1);
     public void emit_back_strike_backward() => back_strike_backward.Emit(1);
     public void emit_back_strike_forward () => back_strike_forward.Emit(1);
+    public void emit_ground_slam_hop_first() => ground_slam_hop_first.Emit(1);
+    public void emit_ground_slam_hop_second() => ground_slam_hop_second.Emit(1);
     public void emit_bite(){
         top_bite.Emit(1);
         bottom_bite.Emit(1);
@@ -22,6 +26,8 @@ public class CavalryParticlesHandler : ParticlesHandler{
         back_strike_forward     .GetComponent<ParticleSystemRenderer>().flip = left;
         top_bite                .GetComponent<ParticleSystemRenderer>().flip = left;
         bottom_bite             .GetComponent<ParticleSystemRenderer>().flip = left;
+        ground_slam_hop_first   .GetComponent<ParticleSystemRenderer>().flip = left;
+        ground_slam_hop_second  .GetComponent<ParticleSystemRenderer>().flip = left;
     }
 
     public override void flip_right(){
@@ -30,5 +36,7 @@ public class CavalryParticlesHandler : ParticlesHandler{
         back_strike_forward     .GetComponent<ParticleSystemRenderer>().flip = right;
         top_bite                .GetComponent<ParticleSystemRenderer>().flip = right;
         bottom_bite             .GetComponent<ParticleSystemRenderer>().flip = right;
+        ground_slam_hop_first   .GetComponent<ParticleSystemRenderer>().flip = right;
+        ground_slam_hop_second  .GetComponent<ParticleSystemRenderer>().flip = right;   
     }
 }
--- Astral Peak/Assets/Source/CavalryRangedCombatHandler.cs
+++ Astral Peak/Assets/Source/CavalryRangedCombatHandler.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 public class CavalryRangedCombatHandler : MonoBehaviour{
     public event Action<GameObject> fetch_sword_fired;
     [SerializeField] Turret
-        bite_3_beam_turret,
-        fetch_sword_turret,
-        sword_summon_turret_1,
-        sword_summon_turret_2,
-        sword_summon_turret_3,
-        sword_summon_turret_4,
-        sword_summon_turret_5;
+        bite_3_beam,
+        fetch_sword,
+        sword_summon_1,
+        sword_summon_2,
+        sword_summon_3,
+        sword_summon_4,
+        sword_summon_5,
+        ground_slam_1,
+        ground_slam_2;
 
     void Start() => link();
     void OnDestroy() => unlink();
     
-    public void fetch_sword_fire_once() => fetch_sword_turret.fire_once();
-    public void bite_3_beam_fire_once() => bite_3_beam_turret.fire_once();
+    public void fetch_sword_fire_once() => fetch_sword.fire_once();
+    public void bite_3_beam_fire_once() => bite_3_beam.fire_once();
+    
+    public void ground_slams(){
+        ground_slam_1.fire_once();
+        ground_slam_2.fire_once();
+    }
+    
     public void summon_swords(){
-        sword_summon_turret_1.fire_once();
-        sword_summon_turret_2.fire_once();
-        sword_summon_turret_3.fire_once();
-        sword_summon_turret_4.fire_once();  
-        sword_summon_turret_5.fire_once();
+        sword_summon_1.fire_once();
+        sword_summon_2.fire_once();
+        sword_summon_3.fire_once();
+        sword_summon_4.fire_once();  
+        sword_summon_5.fire_once();
     }
 
-    void link() => fetch_sword_turret.projectile_fired += foo;
+    void link() => fetch_sword.projectile_fired += foo;
     public void foo(GameObject x){fetch_sword_fired?.Invoke(x);}
-    void unlink() => fetch_sword_turret.projectile_fired -= fetch_sword_fired;
+    void unlink() => fetch_sword.projectile_fired -= fetch_sword_fired;
 }
