46c695ef687ef71c5517a5edb7d9a1646e50bb81
naserra <naserra.work@gmail.com>
2025-02-06 16:25:37 +0000

    added yell projectile attack to final boss phase 2. tweaked  yell particle colour to be less opaque.

--- Astral Peak/Assets/Source/Giant2.cs
+++ Astral Peak/Assets/Source/Giant2.cs
@@ -1,44 +1,73 @@
 using UnityEngine;
 using Entropek;
 using System;
-using Unity.VisualScripting;
+
+using System.Collections.Generic;
+using Entropek.Collections;
 
 public class Giant2 : Boss<Movement>{
     Coroutine idle_state;
     [Header("Giant2")]
     [SerializeField] FinalBossRoomGroundHandler ground_handler;
     private int HEAD_LAYER = 0, LHAND_LAYER = 1, RHAND_LAYER = 2;
+    // which animation layer to be used for each attack.
+    private HashSet<string> 
+        head_attacks = new(){
+            "Giant2YellProjectiles"
+        },
+        hand_attacks = new(){
+
+        };
+    // what behaviour occurs when calling said attack.
+    private HashSet<string>
+        move_to = new(){
+
+        },
+        idle_fly = new(){
+            "Giant2YellProjectiles"
+        };
 
     void Awake(){
         sound.set_functions(new GiantSound(gameObject));
+        state = new StateQueue(this, fly_and_attack_state);
         link_events();
     }
     void Start(){
         set_phase_data("phase_1");
         //switch_phase();
-        //idle(3);
+        idle(1);
         movement.figure_eight_state(reverse: false, x_factor:.1f, y_factor:.05f);
     }
 
-    private void idle(){
+    private void fly_and_attack_state(){
         animator.Play("Giant2Idle", HEAD_LAYER);
         animator.Play("Giant2Idle", LHAND_LAYER);
         animator.Play("Giant2Idle", RHAND_LAYER);
-        no_state();
+        combat.chose_attack_state(target);
     }
 
-    private void stop_idle_loop(){
-        if(idle_state != null)
-            StopCoroutine(idle_state);
+    protected override void attack(BossAttack attack){
+        string animation = attack.animation_id;
+        if(move_to.Contains(animation)){
+            // do stuff.
+        }
+        if(hand_attacks.Contains(animation))
+            state.queue_and_start(time: animator.get_clip_length(animation),start_action:()=>animator.Play(animation,UnityEngine.Random.Range(1,3)));
+        else
+            state.queue_and_start(time: animator.get_clip_length(animation),start_action:()=>animator.Play(animation,HEAD_LAYER));
+        combat.halt();
     }
 
     private void idle(float time){
-        stop_idle_loop();
-        idle_state = StartCoroutine(Util.timer(
-            time,
-            start_action:()=>idle(),
-            time_out:()=>follow_and_attack_state()
-        ));
+        state.queue_and_start(
+            time: time,
+            start_action: ()=>{
+                animator.Rebind();
+                animator.Play("Giant2Idle", HEAD_LAYER);
+                animator.Play("Giant2Idle", LHAND_LAYER);
+                animator.Play("Giant2Idle", RHAND_LAYER);
+            }
+        );
     }
 
     int get_current_ground_piece(){
@@ -48,6 +77,8 @@ public class Giant2 : Boss<Movement>{
             Int32.TryParse(other.name, out x);
         return x;
     }
+
+    public void projectile_yell_camera_shake() => CameraController.instance.shake_camera(4f,.7f, true);
 
     protected void link_events(){
         link_game_manager();
--- Astral Peak/Assets/Source/Giant2Combat.cs
+++ Astral Peak/Assets/Source/Giant2Combat.cs
@@ -2,9 +2,27 @@ using System;
 using System.Collections.Generic;
 
 public class Giant2Combat : BossCombat{
+    BossAttack
+    yell_projectile = new BossAttack(
+        "Giant2HeadYellProjectile",
+        chance:                 50,
+        max_player_distance:    15,
+        min_player_distance:    0,
+        arena_bound_distance:   0,
+        attack_cooldown:        2,
+        idle_cooldown:          0,
+        combat_cooldown:        4
+    )
+    ;
+
     protected override void create_movesets(){
         movesets = new Dictionary<string, Action>(){
-            {"phase_1",()=>{}}
+            {"phase_1",()=>{
+                    special_moveset = new List<BossAttack>(){
+                        yell_projectile
+                    };
+                }
+            }
         };
     }
 }
