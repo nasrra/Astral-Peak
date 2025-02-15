22631cb547b33a75db73bc8557d0cceeb141c8d2
naserra <naserra.work@gmail.com>
2025-02-15 11:20:24 +0000

    initial commit

---
+++ Astral Peak/Assets/Source/MusicSounds.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+namespace Sounds{
+
+public class MusicSound : Sound{
+    public MusicSound(SoundID _id, string _clip, float _volume) 
+    : base(_id, _clip, AudioManager.sfx_mixer, _volume, _cached: false, _diegetic: true, _randomize_pitch: false, _min_pitch: 1f, _max_pitch: 1f){}
+}
+
+public class WolfBossMusic1 : MusicSound{
+    public WolfBossMusic1() : base(
+        _id:                SoundID.WOLF_BOSS_MUSIC_1,
+        _clip:              "abrn_run_1",
+        _volume:            .75f
+    ){}    
+}
+
+public class WolfBossMusic2 : MusicSound{
+    public WolfBossMusic2() : base(
+        _id:        SoundID.WOLF_BOSS_MUSIC_2,
+        _clip:      "abrn_run_2",
+        _volume:    .75f
+    ){}    
+}
+
+public class MageBossMusic1 : MusicSound{
+    public MageBossMusic1() : base(
+        _id:        SoundID.MAGE_BOSS_MUSIC_1,
+        _clip:      "abrn_red_dress_1",
+        _volume:    .75f
+    ){}    
+}
+
+public class MageBossMusic2 : MusicSound{
+    public MageBossMusic2() : base(
+        _id:        SoundID.MAGE_BOSS_MUSIC_2,
+        _clip:      "abrn_red_dress_2",
+        _volume:    .75f
+    ){}    
+}
+
+public class AltarMusic : MusicSound{
+    public AltarMusic() : base(
+        _id:        SoundID.ALTAR_MUSIC,
+        _clip:      "altar_theme",
+        _volume:    .6f
+    ){}    
+}
+
+public class Introduction : MusicSound{
+    public Introduction() : base(
+        _id:        SoundID.INTRODUCTION_MUSIC,
+        _clip:      "introduction",
+        _volume:    .75f
+    ){}    
+}
+
+public class DomineMusic : MusicSound{
+    public DomineMusic() : base(
+        _id:        SoundID.DOMINE_MUSIC,
+        _clip:      "domine(faster)",
+        _volume:    1f
+    ){}    
+}
+
+}
---
+++ Astral Peak/Assets/Source/SfxSounds.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Sounds{
+public class SfxSound : Sound{
+    public SfxSound(SoundID _id, string _clip, float _volume, bool _diegetic, bool _randomize_pitch, float _min_pitch = 1, float _max_pitch = 1) 
+    : base(_id, _clip,  AudioManager.sfx_mixer, _volume, true, _diegetic, _randomize_pitch, _min_pitch, _max_pitch){}
+}
+
+
+public class None : SfxSound{
+    public None() : base(
+        _id:                SoundID.NONE,
+        _clip:              "silence",
+        _volume:            0f,
+        _diegetic:          false,
+        _randomize_pitch:   false,
+        _max_pitch:         0f,
+        _min_pitch:         0f
+    ){}    
+}
+
+
+}
