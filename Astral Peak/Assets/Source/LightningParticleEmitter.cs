b2dcb19cd88da2f56b4ac389860a2938ad5e6905
naserra <naserra.work@gmail.com>
2025-03-12 13:04:06 +1100

    added spark particles to lightning strikes

--- Astral Peak/Assets/Source/LightningParticleEmitter.cs
+++ Astral Peak/Assets/Source/LightningParticleEmitter.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class LightningParticleEmitter : LineParticleEmitter{
     [SerializeField] AudioPlayer audio_player;
+    [SerializeField] ParticleSystem end_point_particles;
     bool is_emitting = false;
 
     protected override void emitted(){
@@ -10,16 +11,19 @@ public class LightningParticleEmitter : LineParticleEmitter{
         CameraController.instance.shake_camera(.33f, .75f, false);
         SceneLighting.instance.set_intensity("global", 3f);
         SceneLighting.instance.reset_lighting("global", .2f);
+        end_point_particles.Emit(7);
     }
 
     protected override void emitting(){
         audio_player.play_diegetic_loop("electricity_crackle_harsh");
         is_emitting = true;
+        end_point_particles.Play();
     }
 
     protected override void ended(){
         if(is_emitting == true){
             audio_player.stop_all_loops();
+            end_point_particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
             is_emitting = false;
         }
     }
