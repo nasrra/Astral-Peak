5a1a672e53bdfa4acd5200f7f1a7a618d8c521f4
naserra <naserra.work@gmail.com>
2025-03-14 19:07:56 +0000

    changes again...

--- Astral Peak/Assets/Source/Cavalry.cs
+++ Astral Peak/Assets/Source/Cavalry.cs
@@ -29,15 +29,16 @@ public class Cavalry : Boss<CavalryMovement>{
     }   
 
     protected override void death_start(){
-        animator.PlayInstant("WolfDeath");
         no_state();
         if(idle_state != null)
             StopCoroutine(idle_state);
-        enable_body_colliders(0);
         stop_all();
+        unlink_events();
+        animator.PlayInstant("WolfDeath");
         movement.zero_velocity(); // stop velocity in case the boss is dashing.
         particles.play_particle("death_ambience");
         sprites.play_death_effect(2f);
+        enable_body_colliders(0);
         base.death_start();
         StartCoroutine(Util.timer(
             animator.get_clip_length("WolfDeath")+3,
