1d68221407e3a50cdf9bb2ebb286b054b163de5a
naserra <naserra.work@gmail.com>
2025-03-14 17:21:27 +0000

    v0.1 for real for real this time

--- Astral Peak/Assets/Source/DomineDoor.cs
+++ Astral Peak/Assets/Source/DomineDoor.cs
@@ -58,7 +58,7 @@ public class DomineDoor : MonoBehaviour{
         Player.instance.animator.force_idle();
         Player.instance.sprite.fade_to_black();
         Player.instance.sprite.enter_domine_door_layer();
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(6);
         CustomSceneManager.load_scene_with_transitions("GameEndStinger");
         yield break;
     }
